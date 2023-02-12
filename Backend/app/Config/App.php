3fd8e149e044976afaf80536eecfc2c1e3d74db4
saurabh yadav <72351102+saurabhyadav01@users.noreply.github.com>
2023-02-12 20:44:59 +0530

    Backend added

--- Backend/app/Config/App.php
+++ Backend/app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/data-search/';
+    public $baseURL = 'http://localhost/';
 
     /**
      * --------------------------------------------------------------------------
